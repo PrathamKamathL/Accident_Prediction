4f4aab4119485758b86c77a0c94b5e225e43583c
Poorvi <pjnayak2003@gmail.com>
2026-04-08 21:05:25 +0530

    Added logic to replace blanks with unknowns and int value 1 for number of casualties

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from app.schema import AccidentInput
 from src.predict import predict
+import pickle
+
+with open("models/defaults.pkl", "rb") as f:
+    defaults = pickle.load(f)
 
 app = FastAPI()
 
@@ -18,14 +22,17 @@ def read_root():
 def predict_accident(data: AccidentInput):
     try:
         input_dict = data.dict()
+        for key, value in input_dict.items():
+            if key == "Number_of_casualties" and (value is None or value==0):
+                input_dict[key] = 1
+            if isinstance(value, str) and value.strip() == "":
+                input_dict[key] = defaults.get(key)
         prediction = predict(input_dict)
         predicted_label = severity_mapping.get(prediction, "Unknown")
 
         return {
             "prediction": predicted_label,
-            "class_id": prediction   # optional but useful
+            "class_id": prediction
         }
     except Exception as e:
-        return {
-            "error": str(e)
-        }
+        raise HTTPException(status_code=500, detail=str(e))
