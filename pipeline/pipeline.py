55121d1778c878aed92973b4c9bb365ff3caee48
PrathamKamathL <kamathpratham57@gmail.com>
2026-04-13 10:08:38 +0530

    Data loader updated and logs updated successfully

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,15 +3,27 @@ from src.preprocess import preprocess_data
 from src.features import feature_engineering
 from src.train import *
 from src.evaluate import *
-
+import logging
 def ml_pipeline():
     rawdata = data_loader()
+    logging.basicConfig(
+        filename='../logs/app.log',
+        filemode='a',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
     preprocessed_data = preprocess_data(rawdata)
+    logging.info("Preprocessing Successful")
     featureEngineeredData = feature_engineering(preprocessed_data)
+    logging.info("Feature Engineering Successful")
     model_v1 = training_decisionTree(featureEngineeredData)
+    logging.info("Training Successful for decision tree")
     model_v2 = training_randomForest(featureEngineeredData)
+    logging.info("Training Successful for random forest")
     classification_report_decision_tree(model_v1, featureEngineeredData)
+    logging.info("Classification Report Successful for decision tree")
     classification_report_random_forest(model_v2, featureEngineeredData)
+    logging.info("Classification Report Successful for random forest")
     print("Pipeline completed execution successfully")
 
 if __name__ == "__main__":
--- src/evaluate.py
+++ src/evaluate.py
@@ -1,11 +1,20 @@
 from sklearn.metrics import classification_report, confusion_matrix
 from src.train import *
 from src.features import *
+import logging
 def classification_report_decision_tree(model_v1, featureEngineeredData):
     x_test = featureEngineeredData["x_test"]
     y_test = featureEngineeredData["y_test"]
     model = model_v1
     y_pred = model.predict(x_test)
+    logging.basicConfig(
+        filename='../logs/app.log',
+        filemode='a',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    logging.info("Classification Report")
+    logging.info(classification_report(y_test, y_pred))
     print(classification_report(y_test, y_pred))
 
 def classification_report_random_forest(model_v2, featureEngineeredData):
